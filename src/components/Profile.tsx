cbd665bf26ea3d115a27a979c313627ddeace848
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-02-24 02:51:36 -0300

    Profile components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/gabrielrmodesto.png" alt="Gabriel Modesto"/>
+            <div>
+                <strong>Gabriel Modesto</strong>
+                <p>Level 1</p>
+            </div>    
+        </div>
+    );
+}
