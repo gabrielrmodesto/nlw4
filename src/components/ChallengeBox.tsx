fcb3285796edccee26ce54cc589467f7bb529430
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-02-25 10:37:26 -0300

    CHallenge box started

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+    return (
+        <div className={styles.challengeBoxContainer}>
+            <div className={styles.challengeNotActive}>
+                <strong>Finalize um ciclo para receber um desafio</strong>
+                <p><img src="icons/level-up.svg" alt="Level up"/></p>
+            </div>
+        </div>
+    )
+}
